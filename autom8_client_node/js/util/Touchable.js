66d95bb304fb2117ea1ba8fb6828ed826b2ca79c
clangen <casey.langen@gmail.com>
2012-10-14 21:09:07 -0700

    More bug fixes for Touchable.

--- autom8_client_node/js/util/Touchable.js
+++ autom8_client_node/js/util/Touchable.js
@@ -1,65 +1,79 @@
-autom8.Touchable = {
-  add: function(rootSelector, itemSelector, clickHandler) {
-    var touchSupported = !!document.createTouch;
-    var startEvent = touchSupported ? "touchstart" : "mousedown";
-    var moveEvent = touchSupported ? "touchmove" : "mousemove";
-    var endEvent = touchSupported ? "touchend touchcancel" : "mouseup mouseout";
-    var started = false;
-    var touchedAdded = false;
+autom8.Touchable = (function() {
+  var touchSupported = !!document.createTouch;
+  var startEvent = touchSupported ? "touchstart" : "mousedown mouseenter";
+  var moveEvent = touchSupported ? "touchmove" : "mousemove";
+  var endEvent = touchSupported ? "touchend touchcancel" : "mouseup mouseleave";  
 
-    var startX = 0, startY = 0;
-    var endX = 0, endY = 0;
-    $(rootSelector).delegate(itemSelector, startEvent, function(e) {
-      var target = $(e.currentTarget);
-      if (!started) {
-        started = true;
+  return {
+    add: function(rootSelector, itemSelector, clickHandler) {
+      var started = null;
+      var startX = 0, startY = 0;
+      var endX = 0, endY = 0;
 
-        if (touchSupported) {
-          startX = endX = e.originalEvent.touches[0].clientX;
-          startY = endY = e.originalEvent.touches[0].clientY;
-        }
-        else {
-          startX = endX = e.originalEvent.clientX;
-          startY = endY = e.originalEvent.clientY;
-        }
-        target.addClass("touched");
-      }
-    });
-
-    $(rootSelector).delegate(itemSelector, moveEvent, function(e) {
-      if (started) {
-        if (touchSupported) {
-          endX = e.originalEvent.touches[0].clientX;
-          endY = e.originalEvent.touches[0].clientY;        
-        }
-        else {
-          endX = e.clientX;
-          endY = e.clientY;      
-        }
-      }
-    });
-
-    $(rootSelector).delegate(itemSelector, endEvent, function(e) {
-      var target = $(e.currentTarget);
-      if (started) {
-        started = false;
-        target.removeClass("touched");
-
-        if (e.type !== "mouseout") {
-          dx = Math.abs(startX - endX);
-          dy = Math.abs(startY - endY);
-
-          if (clickHandler && dx < 10 && dy < 10) {
-            clickHandler(e);
+      /* start */
+      $(rootSelector).delegate(itemSelector, startEvent, function(e) {
+        var target = $(e.currentTarget);
+        if (e.type === "mouseenter") {
+          if (started && e.which === 0) {
+            started = null; /* mouseup not captured, not got an */
+          }
+          if (started === e.currentTarget) {
+            target.addClass("touched");
           }
         }
-      }
-    });
-  },
+        else if (!started) {
+          started = e.currentTarget;
 
-  remove: function(rootSelector, itemSelector) {
-    $(rootSelector).undelegate(itemSelector, 'touchstart');
-    $(rootSelector).undelegate(itemSelector, 'touchmove');
-    $(rootSelector).undelegate(itemSelector, 'touchend touchcancel');
-  }
-};
+          if (touchSupported) {
+            startX = endX = e.originalEvent.touches[0].clientX;
+            startY = endY = e.originalEvent.touches[0].clientY;
+          }
+          else {
+            startX = endX = e.originalEvent.clientX;
+            startY = endY = e.originalEvent.clientY;
+          }
+
+          target.addClass("touched");
+        }
+      });
+
+      /* move */
+      $(rootSelector).delegate(itemSelector, moveEvent, function(e) {
+        if (started) {
+          if (touchSupported) {
+            endX = e.originalEvent.touches[0].clientX;
+            endY = e.originalEvent.touches[0].clientY;        
+          }
+          else {
+            endX = e.clientX;
+            endY = e.clientY;      
+          }
+        }
+      });
+
+      /* end */
+      $(rootSelector).delegate(itemSelector, endEvent, function(e) {
+        var target = $(e.currentTarget);
+        if (started) {
+          target.removeClass("touched");
+
+          if (e.type !== "mouseleave") {
+            started = null;
+            dx = Math.abs(startX - endX);
+            dy = Math.abs(startY - endY);
+
+            if (clickHandler && dx < 20 && dy < 20) {
+              clickHandler(e);
+            }
+          }
+        }
+      });
+    },
+
+    remove: function(rootSelector, itemSelector) {
+      $(rootSelector).undelegate(itemSelector, startEvent);
+      $(rootSelector).undelegate(itemSelector, moveEvent);
+      $(rootSelector).undelegate(itemSelector, endEvent);
+    }
+  };
+}());

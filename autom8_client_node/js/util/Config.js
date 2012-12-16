8b18f6f0cd46066db40ea34790af7763c7a5e222
clangen <casey.langen@gmail.com>
2012-12-16 12:14:32 -0800

    Fixed up config/body class names

--- autom8_client_node/js/util/Config.js
+++ autom8_client_node/js/util/Config.js
@@ -17,11 +17,11 @@ namespace("autom8").Config = (function() {
   if (isIOS) {
     var isIOS6 = /i(Phone|Pod) OS (5|6)/.test(ua) || /Apple-i(Phone|Pod)(5|6)/.test(ua);
     config.display.animations.collapse = isIOS6;
-    config.display.classes.body = 'iPhone';
+    config.display.classes.body = 'iphone';
   }
   else if (isChrome) {
     config.display.animations.collapse = true;
-    config.display.classes.body = 'Chrome';    
+    config.display.classes.body = 'chrome';
   }
 
   $(document).ready(function() {

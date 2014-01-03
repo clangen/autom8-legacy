691aa00f066848f1163c54a04e5a78ed5a02766b
casey langen <casey.langen@gmail.com>
2014-01-02 22:41:27 -0800

    much more complete mock device. supports lamps and security sensors as well

--- libautom8/src/devices/null_device_system.cpp
+++ libautom8/src/devices/null_device_system.cpp
@@ -2,29 +2,109 @@
 #include "device_base.hpp"
 #include "simple_device.hpp"
 
+#include <server.hpp>
+#include <common_messages.hpp>
+
 using namespace autom8;
 
-class null_device: public simple_device {
+class null_device: public simple_device, public lamp, public security_sensor {
 public:
     null_device(
         database_id id,
         const std::string& address,
         const std::string& label,
-		const std::vector<std::string>& groups = std::vector<std::string>())
+		const std::vector<std::string>& groups = std::vector<std::string>(),
+		device_type type = device_type_unknown)
 		: simple_device(id, address, label, groups)
+		, type_(type)
+		, brightness_(100)
+		, armed_(true)
+		, tripped_(false)
 	{
 			
 	}
+	
+	virtual void on_status_changed() {
+		simple_device::on_status_changed();
+		server::send(messages::responses::device_status_updated(shared_from_this()));
+	}
 
+	/* device base */
+	void get_extended_json_attributes(json_value& target) {
+		if (type_ == device_type_lamp) {
+			target["brightness"] = brightness_;
+		}
+		else if (type_ == device_type_security_sensor) {
+			target["armed"] = armed_;
+			target["tripped"] = tripped_;
+		}
+	}
+
+	/* simple device */
     virtual device_type type() {
-		return device_type_lamp;
+		return type_;
 	}
 
 	virtual void turn_on() {
+		if (status_ != device_status_on) {
+			status_ = device_status_on;
+			on_status_changed();
+		}
 	}
 
     virtual void turn_off() {
+		if (status_ != device_status_off) {
+			status_ = device_status_off;
+			on_status_changed();
+		}
 	}
+
+	/* lamp */
+    virtual int brightness() {
+		return brightness_;
+	}
+       
+	virtual void set_brightness(int brightness) {
+		if (brightness != brightness_) {
+			brightness_ = brightness;
+			on_status_changed();
+		}
+    };
+
+	/* security_sensor */
+	virtual void arm() {
+		if (!armed_) {
+			armed_ = true;
+			on_status_changed();
+		}
+	}
+    
+	virtual void disarm() {
+		if (armed_) {
+			armed_ = false;
+			on_status_changed();
+		}
+	}
+
+    virtual void reset() {
+		if (tripped_) {
+			tripped_ = false;
+			on_status_changed();
+		}
+	}
+
+    virtual bool is_armed() {
+		return armed_;
+	}
+
+    virtual bool is_tripped() {
+		return tripped_;
+	}
+
+private:
+	device_type type_;
+	int brightness_;
+	bool armed_, tripped_;
 };
 
 device_ptr null_device_system::null_device_factory::create(
@@ -34,7 +114,7 @@ device_ptr null_device_system::null_device_factory::create(
 	const std::string& label,
 	const std::vector<std::string>& groups)
 { 
-	return device_ptr(new null_device(id, address, label, groups));
+	return device_ptr(new null_device(id, address, label, groups, type));
 }
 
 std::string null_device_system::null_device_factory::name() const {

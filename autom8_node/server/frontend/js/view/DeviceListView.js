50f3257118fdcd1d562da80e04ad1ec396367e1d
casey langen <clangen@ubuntu>
2014-01-16 22:56:12 -0800

    bunch of ui tweaks and fixes

--- autom8_node/server/frontend/js/view/DeviceListView.js
+++ autom8_node/server/frontend/js/view/DeviceListView.js
@@ -1,6 +1,37 @@
  (function() {
   var View = autom8.mvc.View;
 
+  // namespace("autom8").DeviceType = {
+  //   Unknown: -1,
+  //   Lamp: 0,
+  //   Appliance: 1,
+  //   SecuritySensor: 2
+  // };
+
+  // namespace("autom8").CommandType = {
+  //   PowerLine: 0,
+  //   RadioFrequency: 1
+  // };
+
+  // namespace("autom8").DeviceStatus = {
+  //   Unknown: 1,
+  //   Off: 0,
+  //   On: 1
+  // };
+
+  var typeToString = {
+    '-1': 'unknown',
+    '0': 'lamp',
+    '1': 'appliance',
+    '2': 'security sensor'
+  };
+
+  var statusToString = {
+    '-1': 'unknown',
+    '0': 'off',
+    '1': 'on'
+  };
+
   var DeviceListView = View.extend({
     template: 'autom8-View-Devices',
 
@@ -24,11 +55,21 @@
 
       if (model.devices) {
         for (var i = 0; i < model.devices.length; i++) {
+          var d = model.devices[i];
+
+          var normalized = {
+            'label': d.label || "unnamed",
+            'address': d.address,
+            'groups': (d.groups || []).join(", ") || "none",
+            'status': statusToString[d.status || -1],
+            'type': typeToString[d.type === undefined ? -1 : d.type]
+          };
+
           var row = new View({
-            className: i % 2 == 0 ? 'even' : 'odd',
+            className: i % 2 === 0 ? 'even' : 'odd',
             tagName: 'li',
             template: 'autom8-View-DeviceRow',
-            templateParams: model.devices[i]
+            templateParams: normalized
           });
 
           this.addChild(row, {appendToElement: $list});
